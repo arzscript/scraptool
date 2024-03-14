cc21cf9d5a269801fbdbaf085716785500803c17
arzscript <jmldn.en@gmail.com>
2024-03-14 15:44:22 +0700

    update

--- test/main.go
+++ test/main.go
@@ -10,4 +10,5 @@ func main() {
     // Get a greeting message and print it.
     message := tweets.Hello("Gladys")
     fmt.Println(message)
+    tweets.ViewURL1("https://accuweather.com")
 }
--- tweets/tweets.go
+++ tweets/tweets.go
@@ -1,9 +1,32 @@
 package tweets
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gocolly/colly"
+)
 
 func Hello(name string) string {
     // Return a greeting that embeds the name in a message.
     message := fmt.Sprintf("Hi, %v. Welcome!", name)
     return message
 }
+
+func ViewURL1(url string){
+    doc, err := http.Get(url)
+    if err != nil {
+        fmt.Println(err)
+    }
+    fmt.Println(doc)
+}
+
+func ViewURL2(url string){
+    c := colly.NewCollector()
+    c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+        link := e.Attr("href")
+        fmt.Println(link)
+    })
+    c.Visit(url)
+}
+
